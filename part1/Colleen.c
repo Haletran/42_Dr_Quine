d52d3488304f60e89b7249e74d3561f458596c2d
Haletran <bpasquier123@gmail.com>
2025-03-21 09:24:15 +0100

    fix: comments in code not following the norm of the subject

--- part1/Colleen.c
+++ part1/Colleen.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
-char x[]="#include <stdio.h>%cchar x[]=%c%s%c;%c//this is a comment%cvoid xd(){}%cint main() {%c//this is a comment too%cxd();%cprintf(x,10,34,x,34,10, 10, 10, 10, 10, 10, 10, 10);%creturn 0;%c}";
-//this is a comment
+char x[]="#include <stdio.h>%cchar x[]=%c%s%c;%c/*%cthis is a comment%c*/%cvoid xd(){}%cint main() {%c/*%cthis is a comment too%c*/%cxd();%cprintf(x,10,34,x,34,10, 10, 10, 10, 10,10,10, 10, 10, 10, 10, 10);%creturn 0;%c}";
+/*
+this is a comment
+*/
 void xd(){}
 int main() {
-//this is a comment too
+/*
+this is a comment too
+*/
 xd();
-printf(x,10,34,x,34,10, 10, 10, 10, 10, 10, 10, 10);
+printf(x,10,34,x,34,10, 10, 10, 10, 10,10,10, 10, 10, 10, 10, 10);
 return 0;
 }
